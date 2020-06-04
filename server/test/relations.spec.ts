40c835edecd959684e1934d6ee1f457a7865cd47
Thomas Rekittke <betterobjects@mac.com>
2020-06-04 10:24:26 +0200

    before belongsToMany

--- server/test/relations.spec.ts
+++ server/test/relations.spec.ts
@@ -6,7 +6,7 @@ import { Seeder } from '../graph-on-rails/core/seeder';
 import { Entity } from '../graph-on-rails/entities/entity';
 import { EntityAccessor } from '../graph-on-rails/entities/entity-accessor';
 
-xdescribe('Relations', () => {
+describe('Relations', () => {
 
   let gor!:Gor;
   let alpha:Entity, beta:Entity, delta:Entity, gamma:Entity;
@@ -48,5 +48,17 @@ xdescribe('Relations', () => {
     const g2 = await accessor.getItemFromBelongsToChain( { entity:alpha, item:a3 }, "Delta.Gamma", {} );
     expect( g2.name ).toEqual("g2");
   });
+
+  it('should find adjective belongsToMany', async ()=> {
+    const phi = gor.graphx.entities['Phi'];
+    const phi1 = _.first( await phi.resolver.resolveTypes( phi, {}, { filter: { name: { eq: "phi1" } } }, {} ) );
+    expect( phi1.chiIds ).toHaveLength( 2 );
+
+    const chi = gor.graphx.entities['Chi'];
+    const chi1 = _.first( await phi.resolver.resolveTypes( chi, {}, { filter: { name: { eq: "chi1" } } }, {} ) );
+    expect( chi1.phiIds ).toHaveLength( 3 );
+    const chi2 = _.first( await phi.resolver.resolveTypes( chi, {}, { filter: { name: { eq: "chi2" } } }, {} ) );
+    expect( chi2.phiIds ).toHaveLength( 0 );
+  })
 
 })
